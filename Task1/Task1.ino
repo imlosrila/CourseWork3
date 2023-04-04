07b35d0847a2ed9141cd021bc0bb3f363930f261
imlosrila <85941346+imlosrila@users.noreply.github.com>
2023-04-04 13:00:56 +0100

    Task1 Initial Commit
    Added task 1 initial code

--- Task1/Task1.ino
+++ Task1/Task1.ino
@@ -1,28 +1,58 @@
-#define in 23
+// debounce.ino 
+// MIT License (see file LICENSE)
 
-unsigned long highTime;
-int freq;
+// LED is active high
+#define GPIO_LED    12 
+#define GPIO_BUTTON 25
 
+static QueueHandle_t queue;
 
-void setup() 
-{
-  pinMode(in, INPUT);
-  int freq;
-  Serial.begin(9600);
-
+//
+// Button Debouncing task:
+//
+static void debounce_task(void *argp) {
+  uint32_t level, state = 0, last = 0xFFFFFFFF;
+  uint32_t mask = 0x7FFFFFFF;
+  bool event;
+  
+  for (;;) {
+    level = !!digitalRead(GPIO_BUTTON);
+    state = (state << 1) | level;
+    if ( (state & mask) == mask
+      || (state & mask) == 0 ) {
+      if ( level != last ) {
+        event = !!level;
+        if ( xQueueSendToBack(queue,&event,1) == pdPASS )
+          last = level;
+      }
+    }
+    taskYIELD();
+  }
 }
 
-void loop() 
-{
+//
+// LED queue receiving task
+//
+static void led_task(void *argp) {
+  BaseType_t s;
+  bool event, led = false;
   
-  highTime = pulseIn(in, HIGH, 1 );
-  if (highTime < 2 )
-  {
-    Serial.println("Too low");
+  // Light LED initially
+  digitalWrite(GPIO_LED,led);
+
+  for (;;) {
+    s = xQueueReceive(
+      queue,
+      &event,
+      portMAX_DELAY
+    );
+    assert(s == pdPASS);
+    if ( event ) {
+      // Button press:
+      // Toggle LED
+      led ^= true;
+      digitalWrite(GPIO_LED,led);
+    }
   }
-
-  freq = 1/(highTime *2 * 0.000001);
-
-  Serial.println(freq);
 }
 
