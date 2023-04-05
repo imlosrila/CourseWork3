4f34383fa22560d3ef4d9087030095e8547b6f63
imlosrila <85941346+imlosrila@users.noreply.github.com>
2023-04-05 16:05:54 +0100

    All task working together
    Task from assignment 2 are working well together, but the timing for each were not set yeat but output was obtained

--- combinedRTOS/FreeRTOS/FreeRTOS.ino
+++ combinedRTOS/FreeRTOS/FreeRTOS.ino
@@ -1,6 +1,64 @@
 // IO pins setup
-#define ledPin 21
-#define buttonPin 22
+// ===============Button/LED===============//
+
+#define buttonPin 33
+#define ledPin 25
+
+int buttonState = LOW; //this variable tracks the state of the button, low if not pressed, high if pressed
+int ledState = -1; //this variable tracks the state of the LED, negative if off, positive if on
+
+long lastDebounceTime = 0;  // the last time the output pin was toggled
+long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
+// ===============Task 1===============//
+#define outT1 21                   // Pin 21 
+int step = 0;                      // Checking wich step to use within the ifs
+int done = 0;                      // Flag when all the tasks is done
+
+
+unsigned long startTime = 0;      // These are the timings in microseconds
+const long interval1 = 200;
+const long interval2 = 50;
+const long interval3 = 30; 
+
+
+unsigned long timeNow = 0;        // Initialization for timestamps 
+unsigned long timeNow2 = 0;
+unsigned long timeNow3 = 0;
+
+
+// ===============Task 2===============// 
+#define inT2 22                    // pin 22 for Task 2
+
+unsigned long highTimeT2;          // Half of wavelength
+int freqT2;                        // Variable that holds the frequency 
+bool stateT2;                      // Checking the state at which the analog read enters
+
+// ===============Task 3===============//
+#define inT3 23                    // pin 23 for task 3
+
+unsigned long highTimeT3;          // Half og wavelength
+int freqT3;                        // Variable that holds the frequency
+bool stateT3;                      // Checking the state at which the analof read enters
+
+// ===============Task 4===============//
+
+// Potentiometer is connected to GPIO 34 (Analog ADC1_CH6) 
+#define potPin 34
+#define led 27                            
+
+// variable for storing the potentiometer value
+int avgVal = 0;
+int toVal = 0;
+int potVal[4] = {0};
+int count = 0;
+
+// ===============Task 5===============//
+
+// Variables for map of the frequency values
+int x;
+int y;
+
 
 //Setup to create task
 
@@ -11,76 +69,177 @@ typedef struct
   int freq2;
 }Data;
 
-int buttonState = LOW; //this variable tracks the state of the button, low if not pressed, high if pressed
-int ledState = -1; //this variable tracks the state of the LED, negative if off, positive if on
-
-long lastDebounceTime = 0;  // the last time the output pin was toggled
-long debounceDelay = 50;    // the debounce time; increase if the output flickers
-
-
-void setup()
-{
-  //Pins setup
-  pinMode(ledPin, LedOut);
-  pinMode(buttonPin, INPUT);
-  // Create tasks
-  xTaskCreate(
-    task1, // Function name
-    "Task1", // Task name
-    1000, // Stack size
-    1, // Priority
-    NULL // Task Handle
-  );
-  xTaskCreate(task2,"Task2",1000,1,NULL);
-  xTaskCreate(task3,"Task3",1000,1,NULL);
-  xTaskCreate(task4,"Task4",1000,1,NULL);
-  xTaskCreate(task5,"Task5",1000,1,NULL);
-  xTaskCreate(button,"CheckButton",1000,1,NULL);
-  xTaskCreate(led,"Task1",1000,1,NULL);
-}
-
-void loop()
-{
-
-}
 
 void task1(void *parameters)
 {
+  TickType_t xLastWakeTime;
+  const TickType_t xFrequency = 10;
+
+  // Initialise the xLastWakeTime variable with the current time.
+  xLastWakeTime = xTaskGetTickCount();
+
   for(;;)
   {
-    // Task1
+    vTaskDelayUntil( &xLastWakeTime, xFrequency );
+
+    digitalWrite(outT1,HIGH);
+    timeNow = micros();        // get the current time 
+    done = 0;                  // setting done state to 0 meaning tasks are not done yet
+
+    while(done == 0)
+    {
+      if((micros() >= (timeNow + interval1) ) && step == 0)      // Make signal low after 200us
+      {
+       digitalWrite(outT1,LOW);
+        timeNow2 = micros();
+        step = 1;
+      }
+
+      if((micros() >= (timeNow2 + interval2) ) && step == 1)    // make signal high again after 50us
+      {
+        digitalWrite(outT1,HIGH);
+        timeNow3 = micros();
+        step = 2;
+      }
+
+      if((micros() >= (timeNow3 + interval3) ) && step == 2)  // make signal low after 30us
+      {
+        digitalWrite(outT1,LOW);
+        timeNow =  micros();
+        step = 0;
+        done = 1;    // done so that we can get out of this while loop
+      }
+    }
   }
+
 }
 
 void task2(void *parameters)
 {
+  TickType_t xLastWakeTime;
+  const TickType_t xFrequency = 100;
+
+  // Initialise the xLastWakeTime variable with the current time.
+  xLastWakeTime = xTaskGetTickCount();
+
   for(;;)
   {
-    // Task2
+    vTaskDelayUntil( &xLastWakeTime, xFrequency );
+
+    stateT2 = digitalRead(inT2);                     // checks the current state of signal ( high or low )
+
+    highTimeT2 = pulseIn(inT2, !stateT2,3000);       // measures the half wave length of either high or low signal
+
+    freqT2 = 1/(highTimeT2 *2 * 0.000001);           // calculating the frequency 
+
+    if (freqT2 < 333)
+    {
+      // condition for too high or low which lets it to 0
+      freqT2 = 333;
+    }
+
+    if (freqT2 > 1000)
+    {
+      freqT2 = 1000;
+    }
+  
   }
 }
 
 void task3(void *parameters)
 {
+  TickType_t xLastWakeTime;
+  const TickType_t xFrequency = 10;
+
+  // Initialise the xLastWakeTime variable with the current time.
+  xLastWakeTime = xTaskGetTickCount();
+
   for(;;)
   {
-    // Task3
+    vTaskDelayUntil( &xLastWakeTime, xFrequency );
+
+    stateT3 = digitalRead(inT3);                   // checks the current state of signal ( high or low )
+
+    highTimeT3 = pulseIn(inT3, !stateT3,2000);     // measures the half wavelength of either the high or low signal
+
+    freqT3 = 1/(highTimeT3 *2 * 0.000001);         // calculating the frequency 
+
+    if (freqT3 < 500)
+    {
+     // condition for too high or low which lets it to 0
+     freqT3 = 500;
+    }
+
+    if (freqT3 > 1000)
+    {
+     freqT3 = 1000;
+    }
+
   }
 }
 
 void task4(void *parameters)
 {
+  TickType_t xLastWakeTime;
+  const TickType_t xFrequency = 100;
+
+  // Initialise the xLastWakeTime variable with the current time.
+  xLastWakeTime = xTaskGetTickCount();
+
   for(;;)
   {
-    // Task4
+    vTaskDelayUntil( &xLastWakeTime, xFrequency );
+
+    if (count < 4)                        // adds the value to the next array as counter goes up
+    {                                      // as long as the last array is not filled
+      potVal[count] = analogRead(potPin);
+     count ++;
+    }
+
+    else
+    {
+      potVal[0] = analogRead(potPin);     // if last position of the array was used
+      count = 1;                          // this goes back to add the reading to position 1
+                                        // and increment count
+    }
+
+    for (int k = 0; k < 4; k++)           // calculating the sum of the numbers in the array
+    {
+      toVal += potVal[k];
+    }
+
+    avgVal = toVal / 4;                  // calculating the average 
+
+    if (avgVal >= 2047)                  // checks if the pot have reached half of its value
+    {
+      digitalWrite(led, HIGH);
+    }
+    else
+    {
+     digitalWrite(led, LOW);
+    }
+    toVal = 0;                           // resets the value of total sum to 0
   }
 }
 
 void task5(void *parameters)
 {
+  TickType_t xLastWakeTime;
+  const TickType_t xFrequency = 100;
+
+  // Initialise the xLastWakeTime variable with the current time.
+  xLastWakeTime = xTaskGetTickCount();
+
   for(;;)
   {
-    // Task5
+    vTaskDelayUntil( &xLastWakeTime, xFrequency );
+
+    x = map(freqT2, 333, 1000, 0, 99);
+    y = map(freqT3, 500, 1000, 0, 99);
+    Serial.print(x);
+    Serial.print(",");
+    Serial.println(y);
+
   }
 }
 
@@ -113,10 +272,60 @@ void button(void *parameters)
   }
 }
 
-void led(void *parameters)
+void ledOut(void *parameters)
 {
   for(;;)
   {
     // LED light up
   }
+}
+
+void loop()
+{
+
+}
+
+void setup()
+{
+  //Pins setup
+
+  Serial.begin(9600);
+  //Task 1 initialization
+  pinMode(outT1, OUTPUT);
+
+  //Task 2 initialization
+  pinMode(inT2, INPUT);
+  int freqT2;
+  
+  //Task 3 initialization
+  pinMode(inT3, INPUT);
+  int freqT3;
+
+  //Task 4 initialization
+  pinMode(led, OUTPUT);
+
+  //Task 5 initialization
+
+  //Button/Led
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+
+  // Create tasks
+
+  xTaskCreatePinnedToCore(
+    task1, // Function name
+    "Task1", // Task name
+    1024, // Stack size
+    NULL, // Param
+    1, // Priority
+    NULL, // Task Handle
+    1
+  );
+  
+  xTaskCreatePinnedToCore(task2,"Task2",1024,NULL,2,NULL,1);
+  xTaskCreatePinnedToCore(task3,"Task3",1024,NULL,2,NULL,1);
+  xTaskCreatePinnedToCore(task4,"Task4",1024,NULL,1,NULL,1);
+  xTaskCreatePinnedToCore(task5,"Task5",1024,NULL,1,NULL,1);
+  xTaskCreatePinnedToCore(button,"CheckButton",1024,NULL,1,NULL,1);
+  xTaskCreatePinnedToCore(ledOut,"LED",1024,NULL,1,NULL,1);
 }
